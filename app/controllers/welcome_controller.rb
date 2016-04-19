e2c9791f46d8ceddd2c73190f0b8d558258d3d97
Jessica Paczuski <jz.paczuski@gmail.com>
2016-04-19 16:14:19 +0200

    add images and add CA seeds

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    @categories = Shoe.where("price > ?", 100)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -21,14 +21,22 @@ end
 
 
 shoes = [
-["jessica simpson", 120, 5, "Almond toe, stacked heel, side zip closure Interior lining, cushioned insole.", "img", "black", "woman", "aldo", 38, "Boots" ],
-["carla", 90, 5, "Almond toe, stacked heel, side zip closure Interior lining, cushioned insole.", "img", "black", "woman", "aldo", 38, "Sandals" ],
-["carla", 90, 5, "Almond toe, stacked heel, side zip closure Interior lining, cushioned insole.", "img", "black", "woman", "aldo", 38, "Sandals" ],
-["carla", 90, 5, "Almond toe, stacked heel, side zip closure Interior lining, cushioned insole.", "img", "black", "woman", "aldo", 38, "Sandals" ]
+["jessica simpson", 120, 5, "Almond toe, stacked heel, side zip closure Interior lining, cushioned insole.", "boots.jpg", "black", "woman", "aldo", 38, "Boots" ],
+["carla", 90, 5, "Almond toe, stacked heel, side zip closure Interior lining, cushioned insole.", "flats.jpg", "black", "woman", "aldo", 38, "Flats" ],
+["carla", 90, 5, "Almond toe, stacked heel, side zip closure Interior lining, cushioned insole.", "pumps.jpg", "black", "woman", "aldo", 38, "Pumps" ],
+["carla", 90, 5, "Almond toe, stacked heel, side zip closure Interior lining, cushioned insole.", "sandals.jpg", "black", "woman", "aldo", 38, "Sandals" ]
 ]
 
-shoes.each do | name, price, stock, description, image, color, gender, brand, size |
-      shoe = Shoe.create( name: name, price: price, stock: stock, description: description, image: image, color: color, gender: gender, brand: brand, size: size )
+
+shoes.each do | name, price, stock, description, image, color, gender, brand, size, categories|
+  shoe = Shoe.create( name: name, price: price, stock: stock, description: description, image: image, color: color, gender: gender, brand: brand, size: size )
+  categories.split(", ").each do |category_name|
+    if category = Category.find_by( name: category_name)
+    else
+      category = Category.create( name: category_name)
+    end
+    CategoriesShoes.create( shoe: shoe, category: category )
+  end
 end
 
 User.create( name: "carla", email: "frank@ex.com", password: "abcd1234" )
