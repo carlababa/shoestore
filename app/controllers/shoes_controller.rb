0fce6163600458e53b3b7cb2f9801e21bf75a818
Carla Babadopulos <cacababa@gmail.com>
2016-04-19 16:42:42 +0200

    made serach box work

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -1,9 +1,25 @@
 class ShoesController < ApplicationController
   def index
     @shoes = Shoe.all
+    # @shoes = Shoe.search(params[:search])
   end
 
   def show
     @shoe = Shoe.find(params[:id])
+  end
+
+  def index
+
+    if params[:search]
+      shoes = Shoe.where("name LIKE ?", "%#{params[:search]}%")
+      if shoes.present?
+        @shoes = shoes
+      else
+        @shoes = Shoe.all
+      end
+    else
+      @shoes = Shoe.all
+    end
+
   end
 end
