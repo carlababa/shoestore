5f59a07ef3c40f2a5b7465e7a993f37b155d2568
Jessica Paczuski <jz.paczuski@gmail.com>
2016-04-22 13:36:02 +0200

    improve review views

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
   def create
     review_params = params.require( :review ).permit( :message )
 
